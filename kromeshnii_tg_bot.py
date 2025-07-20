87d0fed64b7824493d7051972107903e3b5c863a
superheher <64899574+superheher@users.noreply.github.com>
2025-07-20 10:51:28 +0300

    Added instagram_fixup_telegram_bot submodule.

--- kromeshnii_tg_bot.py
+++ kromeshnii_tg_bot.py
@@ -9,6 +9,9 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
+import instagram_fixup_telegram_bot.ins_to_tg
+instagram_fixup_telegram_bot.ins_to_tg.setup(bot)
+
 if __name__ == '__main__':
     if not TELEGRAM_BOT_TOKEN:
         print("Error: TELEGRAM_BOT_TOKEN not found in .env file")
